3403f96c59cd2afc7d90eb30e8b2839ee23d03b8
muthuri-dev <muthurikennedy082@gmail.com>
2024-04-29 19:01:24 +0300

    feat(Modules):Add posts and users modules with their controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [UsersModule, PostsModule],
 })
 export class AppModule {}
